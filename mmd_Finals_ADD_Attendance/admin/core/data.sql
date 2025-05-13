9af4a5abf6480e12ad6521ecd40da77d8f86eca8
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2025-05-13 08:46:18 +0800

    Delete mmd_Finals_ADD_Attendance directory

--- mmd_Finals_ADD_Attendance/admin/core/data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE attendance_system_users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR (255),
-    password TEXT,
-    first_name VARCHAR (255),
-    last_name VARCHAR (255),
-    is_admin BOOL,
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
